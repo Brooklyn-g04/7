4bdb5b14d362eacf6f4a61386b43f92c9afa7a63
100103067 <100103067@lubbockisd.net>
2019-10-07 15:38:30 -0500

    part 1 of today added

--- 10/7.py
+++ 10/7.py
@@ -8,3 +8,34 @@ y = "Those who know %s and those who %s" % (binary, doNot)
 print(x)
 print(y)
 
+print("I said: %r.:" % x)
+print("I also said: '%s'." % y)
+
+hilarious = True
+jokeEvaluation = "Isn't that joke so funny?! %r"
+print(jokeEvaluation % hilarious)
+
+w = "This is the left side of..."
+e = "a string with a right side."
+print(w+e)
+
+# More printing fun
+print("Mary had a little lamb.")
+print("It's fleece was white as %s." % 'snow')
+print("and everywhere that Mary went.")
+print("." * 10)
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "b"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+print(end1 + end2 + end3 + end4 + end5 + end6,)
+print(end7 + end8 + end9 + end10 + end11 + end12)
