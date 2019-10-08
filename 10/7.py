4c79383d01b63c984aa5525820b06775f1aa5a13
100103067 <100103067@lubbockisd.net>
2019-10-08 15:37:52 -0500

    1st commit of 10/8

--- 10/7.py
+++ 10/7.py
@@ -47,3 +47,48 @@ end12 = "r"
 print(end1 + end2 + end3 + end4 + end5 + end6,)
 print(end7 + end8 + end9 + end10 + end11 + end12)
 print("Cheese burger")
+
+
+# But wait! There's more:
+formatter = "%r %r %r %r"
+print(formatter % (1, 2, 3, 4))
+print(formatter %(True, False, False, True))
+print(formatter % (formatter, formatter, formatter, formatter))
+# why do I use %r instead of %s in the above example?
+# the %r prints everything
+
+# which should I use on a regular basis?
+
+# why does %r sometimes only give me single quotes around things?
+
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApril\nMay\nJun\nJul\nAug"
+
+print("Here are the days: ", days)
+print("Her are the months: ", months)
+
+print("""
+There's something going on here.
+With the three double-quotes
+We'll be able to type as we like.
+Even 4 lines if we want, or 5, or 6.
+""")
+
+print("Here are the months: %r" % months)
+print("Here are the months: %s" % months)
+
+# escape sequences redux
+tabbyCat = "\tI'm tabbed in."
+persianCat = "I'm split\non a line."
+backslashCat = "I'm \\ a \\ cat."
+topCat = """
+I'll do a list:
+\t* Cat Food
+\t* Fishies
+\t* Catnip\n\t*Grass
+"""
+
+print(tabbyCat)
+print(persianCat)
+print(backslashCat)
+print(topCat)
